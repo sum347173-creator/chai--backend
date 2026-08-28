cd65650f2ce8b6cf13490e02f00728af1cee676a
Your Name <your@email.com>
2026-08-28 13:45:48 +0530

    setup backend database authentication and models

---
+++ src/index.js
@@ -0,0 +1,56 @@
+
+
+
+import "dotenv/config";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+connectDB()
+ .then(() => {
+
+            app.on("error", (error) => {
+            console.log("ERRR:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(
+                `Server is running at port: ${process.env.PORT || 8000}`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed !!!", err);
+    });
+
+
+
+
+
+/*
+    1 approach to connect database and  start server 
+
+import express from "express";
+const app = express()
+
+
+(async () => {
+    try {
+      await mongoose.connect('${ process.env.MONGODB_URI }/${DB_NAME}')
+      app.on("error",(error) => {
+        console.log("ERRR:", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log('App is listening on port $ {process.env.PORT}');
+    })
+
+    } catch (error) {
+        console.error("ERROR:", error)
+        throw error
+    }
+    
+}) ()
+    */
---
+++ src/models/user.model.js
@@ -0,0 +1,102 @@
+import mongoose , { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type : String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+         email: {
+            type : String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+         fullname: {
+            type : String,
+            required:true,
+            trim:true,
+            index:true
+        },
+            avatar: {
+            type : String, // cloudinary url
+            required: true,
+        },
+            coverImage: {
+            type : String, // cloudinary url
+        },
+            watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type : String,
+            required:[true,'Passward is required']
+        },
+        refreshToken:{
+            type: String
+        }
+        
+
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))return next();
+
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//  check if the provided password is correct
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+// generate access token for the user
+
+userSchema.methods.generateAccessToken = function(){
+      return jwt.sign(
+        {
+        _id : this_id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRES
+        }
+      )
+}
+
+userScheme.methods.generateReferenceToken = function(){
+       
+     return jwt.sign(
+        {
+        _id : this_id,
+        
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRES
+        }
+      )
+}
+
+export const User = mongoose.model("User", userSchema)
