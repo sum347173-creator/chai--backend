13453f0bea3d91702e9faad03b11b3d336650c8f
Your Name <your@email.com>
2026-09-02 09:36:40 +0530

    fix: update refresh token validation logic in refreshAccessToken function
    feat: add subscription model for user channel subscriptions

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose ,{schema} from  "mongoose"
+
+const subscriptionSchema = new  Schema({
+    subscriber: {
+        type: Schema.Type.ObjectId,// one who is subscribing
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Type.ObjectId,// one to whom `subscriber` is subscribing
+        ref: "User"
+    }
+}, { timestamps: true })
+
+
+
+
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
